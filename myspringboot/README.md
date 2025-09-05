cf0816472531f902013bb8cda1415d1eac0cbd18
caramelpopcornKHU <hyungmin8528@naver.com>
2025-09-05 17:45:10 +0900

    create myspringboot

---
+++ myspringboot/README.md
@@ -0,0 +1,3 @@
+# Begin Springboot
+
+src/main/resources/templates밑에 html파일
---
+++ myspringboot/src/main/java/lx/edu/springboot/controller/HelloController.java
@@ -0,0 +1,31 @@
+package lx.edu.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lx.edu.springboot.dao.AddrBookDAO;
+import lx.edu.springboot.vo.AddrBookVO;
+
+@Controller
+public class HelloController {
+
+	@Autowired
+	AddrBookDAO dao;
+	
+
+	@RequestMapping(value = "/hello")
+	public String hello(Model model) throws Exception {
+		AddrBookVO vo = dao.getDB(1);
+		model.addAttribute("vo",vo);
+		return "hello";
+	}
+	
+	
+	// 매트로닉
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "index";
+	}
+}
